f4d014c927d2e0c1ff3b27299bcd683304159ca8
Boyul Kim <boyulkim1@gmail.com>
2020-10-20 11:49:44 -0700

    updated javascript light bulb - boyul kim

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,13 +1,17 @@
 var $container = document.querySelector('.container-light');
 var $lightBulb = document.querySelector('.lightbulb-light');
 
+var switchOn = true;
+
 function lightSwitch() {
-  if ($container.className === 'container-light' && $lightBulb.className === 'lightbulb-light') {
+  if (switchOn === true) {
     $container.className = 'container-dark';
     $lightBulb.className = 'lightbulb-dark';
-  } else if ($container.className === 'container-dark' && $lightBulb.className === 'lightbulb-dark') {
+    switchOn = false;
+  } else if (switchOn === false) {
     $container.className = 'container-light';
     $lightBulb.className = 'lightbulb-light';
+    switchOn = true;
   }
 
 }
