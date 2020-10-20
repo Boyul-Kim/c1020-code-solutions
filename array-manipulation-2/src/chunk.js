e8755ec4bdda294c3bcfde90a007cb412439e627
Boyul Kim <boyulkim1@gmail.com>
2020-10-20 08:48:02 -0700

    worked on array manipulation

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -2,16 +2,30 @@
 function chunk(array, size) {
   var arrayCopy = array;
   var totalArray = [];
+  var counter = 0;
+  var undefinedCounter = 0;
   for (var i = 0; i < (array.length / size); i++) {
     var chunkArray = [];
 
     for (var x = 0; x < array.length / (array.length / size); x++) {
-      chunkArray.push(arrayCopy[x]);
-      delete arrayCopy[x];
+      // debugger;
+      if (undefinedCounter > array.length - 1) {
+        break;
+      } else if (counter >= array.length / (array.length / size)) {
+        chunkArray.push(arrayCopy[x + counter]);
+        // console.log('index test after', x + counter);
+        undefinedCounter++;
+      } else {
+        chunkArray.push(arrayCopy[x]);
+        // console.log('index test before', x);
+        counter++;
+        undefinedCounter++;
+      }
+      // chunkArray.push(arrayCopy[x]);
+      // delete arrayCopy[x];
       // console.log(chunkArray);
     }
     totalArray.push(chunkArray);
   }
-
   return totalArray;
 }
