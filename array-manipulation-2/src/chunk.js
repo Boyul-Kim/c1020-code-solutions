bdbb248e2244b5fb911e3561c4ec9e8a1838dbb7
Boyul Kim <boyulkim1@gmail.com>
2020-10-20 10:14:43 -0700

    completed array manipulation 2 - boyul kim

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -8,22 +8,17 @@ function chunk(array, size) {
     var chunkArray = [];
 
     for (var x = 0; x < array.length / (array.length / size); x++) {
-      // debugger;
       if (undefinedCounter > array.length - 1) {
         break;
       } else if (counter >= array.length / (array.length / size)) {
-        chunkArray.push(arrayCopy[x + counter]);
-        // console.log('index test after', x + counter);
+        chunkArray.push(arrayCopy[counter]);
+        counter++;
         undefinedCounter++;
       } else {
         chunkArray.push(arrayCopy[x]);
-        // console.log('index test before', x);
         counter++;
         undefinedCounter++;
       }
-      // chunkArray.push(arrayCopy[x]);
-      // delete arrayCopy[x];
-      // console.log(chunkArray);
     }
     totalArray.push(chunkArray);
   }
