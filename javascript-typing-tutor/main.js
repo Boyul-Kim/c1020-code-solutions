8714a98f92e36f6b987cc0453f4fb6df450a72d5
Boyul Kim <boyulkim1@gmail.com>
2020-10-21 14:41:44 -0700

    completed javascript typing tutor - boyul kim

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,10 +1,36 @@
 /// var $text = document.querySelector('.text');
-var $type = document.querySelector('.type');
-// console.log($text.textContent[0]);
-// console.dir($text);
+var $body = document.querySelector('body');
+var $span = document.querySelectorAll('span');
+var $accuracy = document.querySelector('.accuracy');
+var $button = document.querySelector('button');
+var counter = 0;
+var miss = 0;
 
 function typeCheck(event) {
-  // console.log(event.key);
+  if (event.key === $span[counter].textContent) {
+    $span[counter].className = 'green';
+    $span[counter].style.textDecoration = 'none';
+    counter++;
+  } else {
+    $span[counter].className = 'red';
+    miss++;
+  }
+
+  $span[counter].style.textDecoration = 'underline';
+
+  if (counter === $span.length - 1) {
+    $accuracy.textContent = 'Accuracy:  ' + Math.round(((counter - miss) / counter * 100)) + '%';
+  }
 }
 
-$type.addEventListener('keydown', typeCheck);
+function reType(event) {
+  counter = 0;
+  miss = 0;
+  for (var i = 0; i <= $span.length - 1; i++) {
+    $span[i].className = '';
+  }
+  $body.addEventListener('keydown', typeCheck);
+}
+
+$body.addEventListener('keydown', typeCheck);
+$button.addEventListener('click', reType);
