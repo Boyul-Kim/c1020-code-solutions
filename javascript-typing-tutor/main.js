cb7051b286cc2f4ea38e4bcc693f2b9b04a4ba3c
Boyul Kim <boyulkim1@gmail.com>
2020-10-21 10:13:17 -0700

    worked on typing tutor

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,10 @@
+/// var $text = document.querySelector('.text');
+var $type = document.querySelector('.type');
+// console.log($text.textContent[0]);
+// console.dir($text);
+
+function typeCheck(event) {
+  // console.log(event.key);
+}
+
+$type.addEventListener('keydown', typeCheck);
