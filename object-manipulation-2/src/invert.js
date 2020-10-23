d87df1d6b7b1bb585d1880b301720ba8be350847
Boyul Kim <boyulkim1@gmail.com>
2020-10-23 00:07:54 -0700

    completed object manipulation 2

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,8 @@
 /* eslint-disable no-unused-vars */
+function invert(source) {
+  var newObject = {};
+  for (var key in source) {
+    newObject[source[key]] = key;
+  }
+  return newObject;
+}
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,7 +1,12 @@
 /* eslint-disable no-unused-vars */
 function pick(source, keys) {
   var newObject = {};
-  for (var key in source) {
-    // console.log(source[key]);
+  for (var key in keys) {
+    for (var sourceKey in source) {
+      if (keys[key] === sourceKey && source[sourceKey] !== undefined) {
+        newObject[sourceKey] = source[sourceKey];
+      }
+    }
   }
+  return newObject;
 }
