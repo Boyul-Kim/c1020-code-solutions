9f3c5ae3b98cc4554ac0aa06e25cf8aa677663b8
Boyul Kim <boyulkim1@gmail.com>
2020-11-03 15:39:03 -0800

    completed javascript-bank exericise - boyul kim

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -4,14 +4,14 @@
 function Account(number, holder) {
   this.number = number; // unique identifier
   this.holder = holder; // name of the person
-  this.transaction = []; // holds tranactions objects when a deposit or withdrawal is made on the account
+  this.transactions = []; // holds tranactions objects when a deposit or withdrawal is made on the account
 }
 
 // causes Acccount object to create a new transaction object of type deposit
 Account.prototype.deposit = function (amount) {
   var depositTransaction = new Transaction('deposit', amount); // 'deposit' becomes transaction.deposit and amount becomes transaction.amount
   if (depositTransaction.amount > 0) {
-    this.transaction.push(depositTransaction);
+    this.transactions.push(depositTransaction);
     return true;
   } else {
     return false;
@@ -22,7 +22,7 @@ Account.prototype.deposit = function (amount) {
 Account.prototype.withdraw = function (amount) {
   var withdrawTransaction = new Transaction('withdrawal', amount);
   if (withdrawTransaction.amount > 0) {
-    this.transaction.push(withdrawTransaction);
+    this.transactions.push(withdrawTransaction);
     return true;
   } else {
     return false;
@@ -31,5 +31,18 @@ Account.prototype.withdraw = function (amount) {
 
 // calculates the current available funds in the account object - adds deposits and withdrawals
 Account.prototype.getBalance = function () {
+  var availableFunds = 0;
 
+  if (this.transactions.length === 0) {
+    return 0;
+  }
+
+  for (var i = 0; i <= this.transactions.length - 1; i++) {
+    if (this.transactions[i].type === 'deposit') {
+      availableFunds += this.transactions[i].amount;
+    } else if (this.transactions[i].type === 'withdrawal') {
+      availableFunds -= this.transactions[i].amount;
+    }
+  }
+  return availableFunds;
 };
