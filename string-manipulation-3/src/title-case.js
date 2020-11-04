f372c97a97988a8d70d16ea0cdf4b7b1ca18dd07
Boyul Kim <boyulkim1@gmail.com>
2020-11-03 17:32:34 -0800

    completed string manipulation 3 - boyul kim

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,4 +1,54 @@
 /* eslint-disable no-unused-vars */
 function titleCase(title) {
+  title = title.toLowerCase();
+  var titleArray = title.split(' ');
+  var specialCases = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
+  var newArray = [];
+  for (var i = 0; i <= titleArray.length - 1; i++) {
 
+    if (i !== 0 && specialCases.includes(titleArray[i])) {
+      if (titleArray[i - 1][titleArray[i - 1].length - 1] === ':') {
+        var wordBreakSpecial = titleArray[i].split('');
+        wordBreakSpecial[0] = wordBreakSpecial[0].toUpperCase();
+        newArray.push(wordBreakSpecial.join(''));
+        if (i < titleArray.length - 1) {
+          newArray.push(' ');
+        }
+      } else {
+        newArray.push(titleArray[i]);
+        newArray.push(' ');
+      }
+    } else if (titleArray[i] === 'javascript') {
+      newArray.push('JavaScript');
+      if (i < titleArray.length - 1) {
+        newArray.push(' ');
+      }
+    } else if (titleArray[i] === 'api') {
+      newArray.push('API');
+      if (i < titleArray.length - 1) {
+        newArray.push(' ');
+      }
+    } else {
+      var wordBreak = titleArray[i].split('');
+      wordBreak[0] = wordBreak[0].toUpperCase();
+      newArray.push(wordBreak.join(''));
+      if (i < titleArray.length - 1) {
+        newArray.push(' ');
+      }
+    }
+
+  }
+
+  var revisedWord = newArray.join('');
+
+  if (revisedWord.includes('Javascript:')) {
+    revisedWord = revisedWord.replace('Javascript:', 'JavaScript:');
+  }
+
+  if (revisedWord.includes('-')) {
+    var index = revisedWord.indexOf('-');
+    revisedWord = revisedWord.replace(revisedWord[index + 1], revisedWord[index + 1].toUpperCase());
+  }
+
+  return revisedWord;
 }
