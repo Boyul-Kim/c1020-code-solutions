5f2cda8fc5079a693d13950458bb2eeac469a85c
Boyul Kim <boyulkim1@gmail.com>
2020-11-22 14:42:32 -0800

    made changes per change request

--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -3,17 +3,11 @@ function isVowel(char) {
   var charLower = char.toLowerCase();
   var vowel = ['a', 'e', 'i', 'o', 'u'];
 
-  if (vowel[0] === charLower) {
-    return true;
-  } else if (vowel[1] === charLower) {
-    return true;
-  } else if (vowel[2] === charLower) {
-    return true;
-  } else if (vowel[3] === charLower) {
-    return true;
-  } else if (vowel[4] === charLower) {
-    return true;
-  } else {
-    return false;
+  for (var i = 0; i <= vowel.length - 1; i++) {
+    if (charLower === vowel[i]) {
+      return true;
+    }
   }
+  return false;
+
 }
