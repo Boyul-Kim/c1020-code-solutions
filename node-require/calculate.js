d0e3c8d1359cffd300347faf5052b0b05d0835e4
Boyul Kim <boyulkim1@gmail.com>
2020-11-25 12:15:52 -0800

    completed node-require exercise - boyul kim

---
+++ node-require/calculate.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+const add = require('./add');
+const subtract = require('./subtract.js');
+const multiply = require('./multiply.js');
+const divide = require('./divide.js');
+
+if (process.argv[3] === 'plus') {
+  console.log('result:', add.add(Number(process.argv[2]), Number(process.argv[4])));
+} else if (process.argv[3] === 'minus') {
+  console.log('result:', subtract.subtract(Number(process.argv[2]), Number(process.argv[4])));
+} else if (process.argv[3] === 'times') {
+  console.log('result:', multiply.multiply(Number(process.argv[2]), Number(process.argv[4])));
+} else if (process.argv[3] === 'over') {
+  console.log('result:', divide.divide(Number(process.argv[2]), Number(process.argv[4])));
+}
---
+++ node-require/divide.js
@@ -0,0 +1,3 @@
+module.exports.divide = function (x, y) {
+  return x / y;
+};
