f27fa596044421c4397815bc4982bad65227f558
Boyul Kim <boyulkim1@gmail.com>
2020-11-28 00:08:52 -0800

    user can add a note completed

--- node-notes-cli/main.js
+++ node-notes-cli/main.js
@@ -1,8 +1,16 @@
 /* eslint-disable no-console */
+const fs = require('fs');
 const data = require('./data.json');
 
 if (process.argv[2] === 'read') {
   for (const key in data.notes) {
     console.log(key + ':', data.notes[key]);
   }
+} else if (process.argv[2] === 'create') {
+  data.notes[data.nextId + ''] = process.argv[3];
+  data.nextId++;
+  const dataJson = JSON.stringify(data, null, 2);
+  fs.writeFile('data.json', dataJson, 'utf8', err => {
+    if (err) throw err;
+  });
 }
