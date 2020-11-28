73ddc0affe6f9db8c2e558086b6c3f6bc57526a5
Boyul Kim <boyulkim1@gmail.com>
2020-11-28 14:13:19 -0800

    completed node notes cli exercise - boyul kim

--- node-notes-cli/main.js
+++ node-notes-cli/main.js
@@ -9,6 +9,16 @@ if (process.argv[2] === 'read') {
 } else if (process.argv[2] === 'create') {
   data.notes[data.nextId + ''] = process.argv[3];
   data.nextId++;
+  updateFile();
+} else if (process.argv[2] === 'delete') {
+  delete data.notes[process.argv[3] + ''];
+  updateFile();
+} else if (process.argv[2] === 'update') {
+  data.notes[process.argv[3] + ''] = process.argv[4];
+  updateFile();
+}
+
+function updateFile(json) {
   const dataJson = JSON.stringify(data, null, 2);
   fs.writeFile('data.json', dataJson, 'utf8', err => {
     if (err) throw err;
