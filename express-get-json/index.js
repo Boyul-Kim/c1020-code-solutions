0a15c3243277dde1db318dedd4d0276c47d12a09
Boyul Kim <boyulkim1@gmail.com>
2020-11-30 17:12:52 -0800

    completed express get json exercise - boyul kim

---
+++ express-get-json/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+const arrayObjects = [
+  {
+    id: 1,
+    name: 'Saitama',
+    course: 'Hero 101',
+    grade: 0
+  },
+  {
+    id: 2,
+    name: 'Batman',
+    course: 'Detective 101',
+    grade: 1
+  }
+];
+
+app.get('/api/grades', function (req, res) {
+  res.json(arrayObjects);
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening on port 3000');
+});
