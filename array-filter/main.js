64a66800c14d6896f005b703c41ed8214fc59e07
Boyul Kim <boyulkim1@gmail.com>
2020-12-07 11:02:21 -0800

    completed change requests

--- array-filter/main.js
+++ array-filter/main.js
@@ -23,5 +23,5 @@ console.log(overFive);
 const startWithE = names.filter(name => name.startsWith('E'));
 console.log(startWithE);
 
-const haveD = names.filter(haveD => haveD.includes('D') || haveD.includes('d'));
+const haveD = names.filter(name => name.includes('D') || name.includes('d'));
 console.log(haveD);
